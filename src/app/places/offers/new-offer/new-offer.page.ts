c659bdfcad475e0298259953a8e0cda2c2cea7a0
Stringnamematt <matttt1717@gmail.com>
2021-11-30 00:34:53 -0800

    cameraworking

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -6,6 +6,26 @@ import { NavController, ToastController, LoadingController } from '@ionic/angula
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
+function base64toBlob(base64Data: any, contentType: any) {
+  contentType = contentType || '';
+  const sliceSize = 1024;
+  const byteCharacters = atob(base64Data);
+  const bytesLength = byteCharacters.length;
+  const slicesCount = Math.ceil(bytesLength / sliceSize);
+  const byteArrays = new Array(slicesCount);
+ 
+  for (let sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+      const begin = sliceIndex * sliceSize;
+      const end = Math.min(begin + sliceSize, bytesLength);
+ 
+      const bytes = new Array(end - begin);
+      for (let offset = begin, i = 0; offset < end; ++i, ++offset) {
+          bytes[i] = byteCharacters[offset].charCodeAt(0);
+      }
+      byteArrays[sliceIndex] = new Uint8Array(bytes);
+  }
+  return new Blob(byteArrays, { type: contentType });
+}
 
 @Component({
   selector: 'app-new-offer',
@@ -58,18 +78,35 @@ export class NewOfferPage implements OnInit, OnDestroy {
           validators: [Validators.required]
           
         }),
+        image: new FormControl(null)
         
     });
   }
 
-  onImagePicked (imageData: string) {
-
+  onImagePicked (imageData: string | File) {
+    let imageFile;
+    if (typeof imageData === 'string') {
+      try {
+        imageFile = base64toBlob(
+          imageData.replace('data:image/jpeg;base64', ''), 
+          'image/jpeg'
+          );
+      } catch (error) {
+          console.log(error);
+          return;
+        }
+      } else {
+        imageFile = imageData;
+    } 
+    this.form.patchValue({image: imageFile });
   }
+
 
   onSubmit() {
     if (!this.form.valid) {
       return;
     }
+    console.log(this.form.value);
     this.loadingCtrl.create( {
       message: 'Creating employee...'
     }).then(loadingEl => {
